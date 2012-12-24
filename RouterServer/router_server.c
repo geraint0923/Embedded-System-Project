b3d9e594d91ebe461ddd9354998ab47a6b4c08e9
geraint0923@gmail.com <geraint0923@gmail.com>
2012-12-25 01:41:11 +0800

    Recover the main file of RouterServer

---
+++ RouterServer/router_server.c
@@ -0,0 +1,66 @@
+#include "mongoose.h"
+#include "query_handler.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <errno.h>
+#include <sys/wait.h>
+
+
+static int exit_flag = 0;
+static struct mg_context *ctx;
+static char server_name[] = "Router Server";
+
+static const char *options[] = {
+	"document_root", "./public",
+	"listening_ports", "8080",
+	NULL
+};
+
+static void signal_handler(int sig_num) {
+	exit_flag = sig_num;
+}
+
+static void *router_server_callback(enum mg_event ev, struct mg_connection *conn) {
+	const struct mg_request_info *ri = mg_get_request_info(conn); 
+	void *ret = NULL;
+	if(ev == MG_NEW_REQUEST) {
+		printf("new connection method:%s  uri:%s\n", ri->request_method, ri->uri);
+		ret = query_handle(conn);
+		printf("0x%08x\n", ret);
+		return ret;
+	}
+
+	return NULL;
+}
+
+static void init_signal() {
+	signal(SIGTERM, signal_handler);
+	signal(SIGINT, signal_handler);
+}
+
+int main(int argc, char **argv) {
+	init_signal();
+	ctx = mg_start(&router_server_callback, NULL, options);
+	if(!ctx) {
+		printf("Null context, failed to start the mongoose server.\n");
+		return -1;
+	}
+	printf("%s started on port(s) %s with web root [%s]\n",
+			server_name, mg_get_option(ctx, "listening_ports"),
+			mg_get_option(ctx, "document_root"));
+
+	while(exit_flag == 0) {
+		sleep(1);
+	}
+	printf("Exiting on signal %d, waiting for all threads to finish...", exit_flag);
+	fflush(stdout);
+	mg_stop(ctx);
+	printf("%s", " done.\n");
+
+	return 0;
+}
