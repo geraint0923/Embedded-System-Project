e39b5b114e99b8360716a661db7c7451aa4af60f
geraint0923@gmail.com <geraint0923@gmail.com>
2012-12-27 17:41:10 +0800

    Add find-phone and vpn response to the server

--- RouterServer/query_handler.c
+++ RouterServer/query_handler.c
@@ -5,6 +5,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
 #include "tunet.h"
 #include "tunet_automator.h"
@@ -101,6 +105,115 @@ static void *query_tunet_user_list(struct mg_connection *conn) {
 	return "";
 }
 
+static void *query_vpn_connect(struct mg_connection *conn) {
+	const struct mg_request_info *ri = mg_get_request_info(conn);
+	char type[BUFFER_LEN];
+	int qlen = strlen(ri->query_string);
+
+	memset(type, 0, BUFFER_LEN);
+
+	mg_get_var(ri->query_string, qlen, "type", type, BUFFER_LEN);
+
+	if(type[0] == '4') {
+		system("/mnt/usb/img/vpn4.sh");
+		mg_printf(conn, HTTP_HEADER_PLAIN
+				"0");
+	} else if(type[0] == '6') {
+		system("/mnt/usb/img/vpn6.sh");
+		mg_printf(conn, HTTP_HEADER_PLAIN
+				"0");
+	} else {
+		mg_printf(conn, HTTP_HEADER_PLAIN
+				"-1");
+	}
+	return "";
+}
+
+static void *query_vpn_disconnect(struct mg_connection *conn) {
+	system("/mnt/usb/img/vpn_clean.sh");
+	mg_printf(conn, HTTP_HEADER_PLAIN
+			"0");
+	return "";
+}
+
+static void *query_vpn_check(struct mg_connection *conn) {
+	FILE *file = fopen("/mnt/usb/img/vpn_exist", "r");
+	int status = 0;
+	if(file) {
+		status = 1;
+	} else {
+		status = 0;
+	}
+	mg_printf(conn,HTTP_HEADER_PLAIN
+			"%d", status);
+	return "";
+}
+
+
+static void udp_broadcast(char *str) {
+	int sock;
+	socklen_t addr_len;
+	struct sockaddr_in server_addr;
+
+	if((sock = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
+		perror("socket");
+		return;
+	}
+	int i = 1;
+	socklen_t len= sizeof(i);
+	setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &i, len);
+	
+
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = inet_addr("192.168.23.255");
+	server_addr.sin_port = htons(33333);  
+	addr_len=sizeof(server_addr);
+	sendto(sock, str, strlen(str), 0, (struct sockaddr*)&server_addr, addr_len);
+
+	close(sock);
+}
+
+static void *query_phone_find(struct mg_connection *conn) {
+	const struct mg_request_info *ri = mg_get_request_info(conn);
+	char name[BUFFER_LEN];
+	char buffer[BUFFER_LEN];
+	int qlen = strlen(ri->query_string);
+
+	memset(name, 0, BUFFER_LEN);
+
+	mg_get_var(ri->query_string, qlen, "name", name, BUFFER_LEN);
+	
+	sprintf(buffer, "find:%s", name);
+	printf("send:%s\n", buffer);
+	udp_broadcast(buffer);
+
+	mg_printf(conn, HTTP_HEADER_PLAIN
+			"0");
+	return "";
+
+}
+
+static void *query_phone_stop(struct mg_connection *conn) {
+	const struct mg_request_info *ri = mg_get_request_info(conn);
+	char name[BUFFER_LEN];
+	char buffer[BUFFER_LEN];
+	int qlen = strlen(ri->query_string);
+
+	memset(name, 0, BUFFER_LEN);
+
+	mg_get_var(ri->query_string, qlen, "name", name, BUFFER_LEN);
+
+	sprintf(buffer, "stop:%s", name);
+	printf("send:%s\n", buffer);
+	udp_broadcast(buffer);
+
+	mg_printf(conn, HTTP_HEADER_PLAIN
+			"0");
+	return "";
+}
+
+
+
 /*
 static void *query_download_add_task(struct mg_connection *conn) {
 	//const struct mg_request_info *ri = mg_get_request_info(conn);
@@ -167,6 +280,31 @@ struct query_handler query_handler_list[] = {
 		.method = "GET",
 		.uri = "/tunet/user_list",
 		.handler = query_tunet_user_list,
+	},
+	{
+		.method = "GET",
+		.uri = "/vpn/connect",
+		.handler = query_vpn_connect,
+	},
+	{
+		.method = "GET",
+		.uri = "/vpn/disconnect",
+		.handler = query_vpn_disconnect,
+	},
+	{
+		.method = "GET",
+		.uri = "/vpn/check",
+		.handler = query_vpn_check,
+	},
+	{
+		.method = "GET",
+		.uri = "/phone/find",
+		.handler = query_phone_find,
+	},
+	{
+		.method = "GET",
+		.uri = "/phone/stop",
+		.handler = query_phone_stop,
 	},
 	/*
 	{
