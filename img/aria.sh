5a8db923b291ff65d391dae2e526d600edd212c1
geraint0923@gmail.com <geraint0923@gmail.com>
2012-12-28 00:20:09 +0800

    Add beta version

---
+++ img/aria.sh
@@ -0,0 +1,1 @@
+aria2c --enable-rpc --rpc-listen-all=true --rpc-allow-origin-all --dir=/mnt/usb/img/public download --save-session=/mnt/usb/img/aria2_unfinish --input-file=/mnt/usb/img/aria2_unfinish -c
