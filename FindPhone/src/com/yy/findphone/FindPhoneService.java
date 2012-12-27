bb09e2e5d712b9a992817968f88a01d1688805db
Yang Yang <geraint0923@gmail.com>
2012-12-27 21:31:31 +0800

    Finish the code on phone.

--- FindPhone/src/com/yy/findphone/FindPhoneService.java
+++ FindPhone/src/com/yy/findphone/FindPhoneService.java
@@ -7,54 +7,79 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
 
+import android.R.integer;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Binder;
 import android.os.IBinder;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
 import android.os.RemoteException;
 
 
 
 public class FindPhoneService extends Service {
 
-	private boolean isListening = false;
+	//private boolean isListening = false;
+	
+	private int packetCount = 0;
 	
 	private Thread listenThread = null;
 	
+	private WakeLock wakeLock;
+	
 	private String name = "newbie";
 	
+	private byte[] recvBuffer = new byte[1024];
+	
 	private final String configPath = "/sdcard/findphone/name";
+	
+	private final String musicPath = "/sdcard/rainbow.mp3";
+	
+	private MediaPlayer mediaPlayer = null;
+	
+	DatagramSocket socket = null;
 	
 	private final IFindPhoneService.Stub binderStub = new IFindPhoneService.Stub() {
 
 		@Override
-		public void setName(String name) throws RemoteException {
+		public void setName(String n) throws RemoteException {
 			// TODO Auto-generated method stub
-			
+			name = n;
 		}
 
 		@Override
 		public String getName() throws RemoteException {
 			// TODO Auto-generated method stub
-			return null;
+			return name;
 		}
 
 		@Override
 		public void startFind() throws RemoteException {
 			// TODO Auto-generated method stub
+			/*
 			isListening = true;
 			listenThread = new Thread(new BroadcastListener());
 			listenThread.start();
+			*/
 		}
 
 		@Override
 		public void stopFind() throws RemoteException {
 			// TODO Auto-generated method stub
+			/*
 			isListening = false;
 			if(listenThread != null) {
 				listenThread.interrupt();
 				listenThread = null;
 			}
+			*/
 		}
 		
 	};
@@ -66,15 +91,70 @@ public class FindPhoneService extends Service {
 			
 			// TODO Auto-generated method stub
 			try {
-				while(isListening) {
-					Thread.sleep(1000);
+				socket = new DatagramSocket(33333);
+//				socket.setBroadcast(true);
+					
+				while(true) {
+					DatagramPacket packet = new DatagramPacket(recvBuffer,recvBuffer.length);
+					socket.receive(packet);
+					String str = new String(recvBuffer, 0, packet.getLength());
+					System.out.println(packetCount+ " "+packet.getLength()+" "+":::" + str);
+					packetCount++;
+					String[] spl = str.split(":");
+					if(spl.length == 2) {
+						if(spl[1].equals(name)) {
+							if(mediaPlayer == null) {
+								new Thread(new Runnable() {
+									
+									@Override
+									public void run() {
+										// TODO Auto-generated method stub
+										mediaPlayer = new MediaPlayer();
+										try {
+											mediaPlayer.setDataSource(musicPath);
+											mediaPlayer.prepare();
+											mediaPlayer.start();
+											Thread.sleep(10000);
+											System.out.print("play");
+											mediaPlayer.stop();
+											mediaPlayer.release();
+											mediaPlayer = null;
+										} catch (IllegalArgumentException e) {
+											// TODO Auto-generated catch block
+											e.printStackTrace();
+										} catch (SecurityException e) {
+											// TODO Auto-generated catch block
+											e.printStackTrace();
+										} catch (IllegalStateException e) {
+											// TODO Auto-generated catch block
+											e.printStackTrace();
+										} catch (IOException e) {
+											// TODO Auto-generated catch block
+											e.printStackTrace();
+										} catch (InterruptedException e) {
+											// TODO Auto-generated catch block
+											e.printStackTrace();
+										}
+									}
+								}).start();
+
+							}
+						}
+					}
+					//handler.sendEmptyMessage(0);
 				}
-			} catch (InterruptedException e) {
+			} catch (SocketException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
 		
 	}
+	
+	
 	
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -99,6 +179,14 @@ public class FindPhoneService extends Service {
 			}
 			
 		}
+		PowerManager pm = (PowerManager)getSystemService(Context.POWER_SERVICE);  
+		wakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "MyWakeLock");  
+		wakeLock.acquire();
+		
+		//isListening = true;
+		listenThread = new Thread(new BroadcastListener());
+		listenThread.start();
+		
 		super.onCreate();
 	}
 	
@@ -116,6 +204,11 @@ public class FindPhoneService extends Service {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		if(listenThread != null) {
+			listenThread.interrupt();
+			listenThread = null;
+		}
+		wakeLock.release();  
 		super.onDestroy();
 	}
 	
--- FindPhone/src/com/yy/findphone/MainActivity.java
+++ FindPhone/src/com/yy/findphone/MainActivity.java
@@ -1,15 +1,120 @@
 package com.yy.findphone;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+import android.net.wifi.WifiManager;
+import android.net.wifi.WifiManager.MulticastLock;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
+import android.os.RemoteException;
+import android.R.integer;
 import android.app.Activity;
+import android.app.Service;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
+	private EditText editText;
+	
+	private Button setNameButton = null;
+	
+	private int count = 0;
+	
+//	private WakeLock wakeLock;
+	
+	DatagramSocket socket = null;
+	
+	private byte[] recvBuffer = new byte[1024];
+	
+	private Handler handler = new Handler() {
+		public void handleMessage(Message msg) {
+            // TODO Auto-generated method stub
+			System.out.println("receive broadcast");
+			count++;
+			editText.setText(count+new String(recvBuffer));
+        }
+	};
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		/*
+		PowerManager pm = (PowerManager)getSystemService(Context.POWER_SERVICE);  
+		wakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "MyWakeLock");  
+		*/
+		
+		/*
+		WifiManager manager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+		MulticastLock lock = manager.createMulticastLock("lock name");
+		lock.setReferenceCounted(true);
+		lock.acquire();
+		*/
+		
+		editText = (EditText)this.findViewById(R.id.edit_text);
+		setNameButton = (Button)this.findViewById(R.id.set_name);
+		
+		setNameButton.setOnClickListener(new Button.OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				String nString = editText.getText().toString();
+				try {
+					findService.setName(nString);
+				} catch (RemoteException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+		});
+		
+		//new Thread(new TestRun()).start();
+		Intent intent = new Intent(MainActivity.this, FindPhoneService.class); 
+		startService(intent); 
+        
+        bindService(intent, serviceConnection, Service.BIND_AUTO_CREATE);  
+	}
+	
+	class TestRun implements Runnable {
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			try {
+				socket = new DatagramSocket(33333);
+				socket.setBroadcast(true);	
+				
+				while(true) {
+					DatagramPacket packet = new DatagramPacket(recvBuffer,recvBuffer.length);
+					socket.receive(packet);
+					handler.sendEmptyMessage(0);
+				}
+			} catch (SocketException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
 	}
 
 	@Override
@@ -18,5 +123,45 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
+	
+	
+	
+	public void onDesotroy() {
+		/*
+		if(socket != null) {
+			socket.close();
+		}
+		wakeLock.release();
+		*/ 
+        
+        this.unbindService(serviceConnection);  
+		super.onDestroy();
+	}
+	
+	private IFindPhoneService findService = null;
+	
+	private ServiceConnection serviceConnection = new ServiceConnection() { 
+
+
+		@Override
+		public void onServiceConnected(ComponentName arg0, IBinder arg1) {
+			// TODO Auto-generated method stub
+			findService = (IFindPhoneService) arg1; 
+			System.out.println("OKK****************");
+			try {
+				editText.setText(findService.getName());
+			} catch (RemoteException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		@Override
+		public void onServiceDisconnected(ComponentName arg0) {
+			// TODO Auto-generated method stub
+			findService = null;
+		} 
+
+	}; 
 
 }
