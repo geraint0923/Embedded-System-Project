dac1253fb572a5c432a34aebcfcf0fc552f38f20
Yang Yang <geraint0923@gmail.com>
2013-01-04 19:53:40 +0800

    Add ipv6 script

---
+++ scripts/ipv6.sh
@@ -0,0 +1,7 @@
+ip tunnel add sit1 mode sit remote 59.66.4.50 local 59.66.133.36
+ifconfig sit1 up
+ifconfig sit1 add 2001:da8:200:900e:0:5efe:59.66.133.36/64
+ip route add 2001::/3 via 2001:da8:200:900e::1 metric 1
+route add -net 59.66.0.0/16 gw 59.66.133.1
+route add -net 166.111.0.0/16 gw 59.66.133.1
+#openvpn --config /etc/openvpn/gerainty/geraint-oy6.ovpn
